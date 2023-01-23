b6071a905c1b455a2dc437400ffda1b78b709c26
horizonvert98 <horizonvert.pu@gmail.com>
2023-01-23 07:44:49 +0200

    extract first h1 tag

--- web-s.py
+++ web-s.py
@@ -20,15 +20,6 @@ from bs4 import BeautifulSoup
 page = requests.get("https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Extract title of page
-page_title = soup.title
-
-# Extract body of page
-page_body = soup.body
-
-# Extract head of page
-page_head = soup.head
-
-# print the result
-print(page_title, page_head)
+# Extract first <h1>(...)</h1> text
+first_h1 = soup.select('h1')[0].text
 
